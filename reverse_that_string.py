f3e789d96b9b479d99960595a0c66d81c81b97ee
Molly <nelsonmlott@gmail.com>
2025-08-05 16:07:08 -0400

    more practice

--- reverse_that_string.py
+++ reverse_that_string.py
@@ -1,3 +1,15 @@
 def reverse_string(s): 
     return s[::-1]
 print(reverse_string("disco ball"))
+
+
+def third_letter(x):
+    return x[3:]
+print(third_letter('ball'))
+
+def reverse_string(s):
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
+print(reverse_string("disco ball"))
